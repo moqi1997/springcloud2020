5d70524bd421502a2aea1e2a05f10d56fc1396b3
moqi1997 <1176323390@qq.com>
2020-04-03 11:21:51 +0800

    微服务项目eureka

--- cloud-consumer-order80/src/main/java/com/moqi/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/moqi/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.moqi.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author moqi
  * @date 2020/4/2 11:43
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
