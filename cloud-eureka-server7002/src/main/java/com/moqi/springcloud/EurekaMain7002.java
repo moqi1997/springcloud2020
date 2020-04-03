4cc7d2f2b2d40fa84af89c765cdb9101554b6e84
moqi1997 <1176323390@qq.com>
2020-04-03 11:11:36 +0800

    微服务项目eureka

--- cloud-eureka-server7002/src/main/java/com/moqi/springcloud/EurekaMain7002.java
+++ cloud-eureka-server7002/src/main/java/com/moqi/springcloud/EurekaMain7002.java
@@ -2,16 +2,16 @@ package com.moqi.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author moqi
- * @date 2020/4/1 14:36
+ * @date 2020/4/3 9:51
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaMain7002 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class,args);
+        SpringApplication.run(EurekaMain7002.class,args);
     }
 }
